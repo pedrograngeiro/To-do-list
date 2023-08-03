e0531ac57db593d1c908612a7f3667d36aff4528
Pedro Grangeiro <pedrop.grangeiro@gmail.com>
2023-08-03 18:05:21 -0300

    Aumentando tamanho check box. E personlizando para preencher com roxo o interior do checkbox quando selecionado

--- js/main.js
+++ js/main.js
@@ -1,42 +1,50 @@
-let grocery = document.getElementById("grocery");
-grocery.addEventListener("submit", addItem);
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', function () {
+        if (this.checked) {
+            this.parentElement.classList.add('checked');
+        } else {
+            this.parentElement.classList.remove('checked');
+        }
+    });
+});
 
-function addItem(e) {
-  e.preventDefault();
-  let data = this.elements.writeList.value;
-  let list = document.querySelector("ol");
-  let item = document.createElement("li");
-  let text = document.createElement("p");
+// function addItem(e) {
+//     e.preventDefault();
+//     let data = this.elements.writeList.value;
+//     let list = document.querySelector('ol');
+//     let item = document.createElement('li');
+//     let text = document.createElement('p');
 
-  text.textContent = data;
-  this.elements.writeList.value = "";
-  item.appendChild(text);
-  list.appendChild(item);
+//     text.textContent = data;
+//     this.elements.writeList.value = '';
+//     item.appendChild(text);
+//     list.appendChild(item);
 
-  let rmvBtn = document.createElement("span");
-  rmvBtn.innerHTML = '<i class="fa-solid fa-minus remove"></i>';
-  item.appendChild(rmvBtn);
+//     let rmvBtn = document.createElement('span');
+//     rmvBtn.innerHTML = '<i class="fa-solid fa-minus remove"></i>';
+//     item.appendChild(rmvBtn);
 
-  let doneBtn = document.createElement("span");
-  doneBtn.innerHTML = '<i class="fa-solid fa-check check"></i>';
-  item.appendChild(doneBtn);
+//     let doneBtn = document.createElement('span');
+//     doneBtn.innerHTML = '<i class="fa-solid fa-check check"></i>';
+//     item.appendChild(doneBtn);
 
-  rmvBtn.addEventListener("click", deleteItem);
-  doneBtn.addEventListener("click", doneItem);
+//     rmvBtn.addEventListener('click', deleteItem);
+//     doneBtn.addEventListener('click', doneItem);
 
-  item.addEventListener("click", doneItem);
-}
+//     item.addEventListener('click', doneItem);
+// }
 
-function deleteItem(e) {
-  this.parentElement.remove();
-}
+// function deleteItem(e) {
+//     this.parentElement.remove();
+// }
 
-function doneItem(e) {
-  let listItem = e.target.parentElement;
-  listItem.classList.toggle("checked");
-}
+// function doneItem(e) {
+//     let listItem = e.target.parentElement;
+//     listItem.classList.toggle('checked');
+// }
 
-function markAsDone(e) {
-  let listItem = e.target.parentElement.parentElement;
-  listItem.classList.add("checked");
-}
+// function markAsDone(e) {
+//     let listItem = e.target.parentElement.parentElement;
+//     listItem.classList.add('checked');
+// }
