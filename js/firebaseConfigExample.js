80e273450c49b18b25b0f61325a1d6ac6adb74ea
Pedro Grangeiro <pedrop.grangeiro@gmail.com>
2023-08-09 15:32:29 -0300

    Configurando firebase.\nCriando modulo para config do firebase.\nAdding firebaseConfig.js a lista de ignore.\nCriando firebaseConfigExample para mostrar base de configuracao

---
+++ js/firebaseConfigExample.js
@@ -0,0 +1,16 @@
+function firebaseConfig() {
+    const firebaseConfig = {
+        apiKey: 'SOME_API_KEY',
+        authDomain: 'SOME_AUTH_DOMAIN',
+        databaseURL: 'SOME_DATABASE_URL',
+        projectId: 'SOME_PROJECT_ID',
+        storageBucket: 'SOME_STORAGE_BUCKET',
+        messagingSenderId: 'SOME_MESSAGING_SENDER_ID',
+        appId: 'SOME_APP_ID',
+        measurementId: 'SOME_MEASUREMENT_ID',
+    };
+
+    return firebaseConfig;
+}
+
+export { firebaseConfig };
