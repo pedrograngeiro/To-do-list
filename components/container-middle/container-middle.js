c281ec49476522d894389c67a8945d2ea545ff4e
Pedro Grangeiro <pedrop.grangeiro@gmail.com>
2023-08-23 15:52:39 -0300

    Implementando card em formato de grid.

--- components/container-middle/container-middle.js
+++ components/container-middle/container-middle.js
@@ -9,3 +9,33 @@ document.addEventListener('DOMContentLoaded', function () {
             console.error('Error loading content:', error);
         });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const selectElement = document.querySelector('.progress');
+    const progressSteps = document.querySelectorAll('.progressbar li');
+
+    selectElement.addEventListener('change', function () {
+        const selectedOption =
+            selectElement.options[selectElement.selectedIndex];
+        const selectedStep = selectedOption.getAttribute('data-foo');
+
+        let foundSelectedStep = false;
+
+        progressSteps.forEach((step) => {
+            if (step.getAttribute('data-step') === selectedStep) {
+                step.classList.add('active');
+                foundSelectedStep = true;
+            } else if (!foundSelectedStep) {
+                step.classList.add('active');
+            } else {
+                step.classList.remove('active');
+            }
+        });
+    });
+    const opcional = document.querySelector('.opcional');
+    const opcionalContainer = document.querySelector('.progress');
+
+    opcional.addEventListener('click', function () {
+        opcionalContainer.classList.toggle('active');
+    });
+});
