43d7302882413f78bbfafe79a22f983efcdd7ba6
Pedro Grangeiro <pedrop.grangeiro@gmail.com>
2023-09-13 17:17:27 -0300

    Recebendo e inserindo os dados no formulario ao clicar no card

--- components/Tarefas/container-main/container-main.js
+++ components/Tarefas/container-main/container-main.js
@@ -1,4 +1,5 @@
 import { listarTarefas, editarTarefa } from '../../../assets/js/crud.js';
+
 document.addEventListener('DOMContentLoaded', function () {
     fetch('components/Tarefas/container-main/container-main.html')
         .then((response) => response.text())
@@ -15,120 +16,108 @@ document.addEventListener('DOMContentLoaded', function () {
                     'container-middle-output'
                 );
 
-                console.log(tarefasDoFirebase);
-
-                editarTarefa(tarefasDoFirebase, 5)
-                    .then((resultado) => {
-                        console.log('Sucesso:', resultado);
-                    })
-                    .catch((erro) => {
-                        console.error('Erro:', erro);
-                    });
-
                 divTarefas.classList.add('grid-container');
                 tarefasDoFirebase.forEach(function (item, index) {
                     const divCard = document.createElement('div');
                     divCard.innerHTML = `
                         <div class="grid-item">
-                        <i id="meuBotao" class="fa-regular fa-pen-to-square"></i>
+                            <i id="meuBotao" class="fa-regular fa-pen-to-square" data-card-index="${index}"></i>
                             <div class='card'>
                                 <h4 id='user_id' class='nome' data-user=''>${
                                     item.autorTarefa
                                 }</h4>
                                 <div class='titulo_e_progress'>
-                                <span id='task' class='titulo_task'>${
-                                    item.nomeTarefa
-                                }</span>
-                                <div class='opcional'>
-                                    <span id='info-task' data-foo='${
-                                        item.statusTarefa
-                                    }'>${item.statusTarefa}</span>
+                                    <span id='task' class='titulo_task'>${
+                                        item.nomeTarefa
+                                    }</span>
+                                    <div class='opcional'>
+                                        <span id='info-task' data-foo='${
+                                            item.statusTarefa
+                                        }'>${item.statusTarefa}</span>
+                                    </div>
+                                </div>
+                                <div class='inicio-e-origem'>
+                                    <p>
+                                        Data Inicio:
+                                        <span id='inicio-data' class='data_span'>${
+                                            item.created
+                                        }</span>
+                                    </p>
+                                    <p>
+                                        Origem:
+                                        <span id='origem' class='origem'>${
+                                            item.origemProjeto
+                                        }</span>
+                                    </p>
+                                </div>
+                                <div id='card-oculto' class='active'>
+                                    <div class='container'>
+                                        <ul class='progressbar'>
+                                            <li data-step='E' ${
+                                                item.statusTarefa === 'E'
+                                                    ? ' class="atual"'
+                                                    : ''
+                                            }></li>
+                                            <li data-step='D' ${
+                                                item.statusTarefa === 'D'
+                                                    ? ' class="atual"'
+                                                    : ''
+                                            }></li>
+                                            <li data-step='H' ${
+                                                item.statusTarefa === 'H'
+                                                    ? ' class="atual"'
+                                                    : ''
+                                            }></li>
+                                            <li data-step='P' ${
+                                                item.statusTarefa === 'P'
+                                                    ? ' class="atual"'
+                                                    : ''
+                                            }></li>
+                                        </ul>
+                                    </div>
+                                    <p>  
+                                        <span id='mensagem-task'>${
+                                            item.mensagem
+                                        }</span>
+                                    </p>
+                                    <p>${
+                                        item.update !== undefined
+                                            ? `<p>
+                                            Data Atualização:
+                                            <span id='atualizacao-data' class='data_span'>${item.update}</span>
+                                        </p>`
+                                            : ''
+                                    }</p>
                                 </div>
                             </div>
-                            <div class='inicio-e-origem'>
-                                <p>
-                                    Data Inicio:
-                                    <span id='inicio-data' class='data_span'>${
-                                        item.created
-                                    }</span>
-                                </p>
-                                <p>
-                                    Origem:
-                                    <span id='origem' class='origem'>${
-                                        item.origemProjeto
-                                    }</span>
-                                </p>
-                            </div>
-                            <div id='card-oculto' class='active'>
-                                <div class='container'>
-                                    <ul class='progressbar'>
-                                        <li data-step='E' ${
-                                            item.statusTarefa === 'E'
-                                                ? ' class="atual"'
-                                                : ''
-                                        }></li>
-                                        <li data-step='D' ${
-                                            item.statusTarefa === 'D'
-                                                ? ' class="atual"'
-                                                : ''
-                                        }></li>
-                                        <li data-step='H' ${
-                                            item.statusTarefa === 'H'
-                                                ? ' class="atual"'
-                                                : ''
-                                        }></li>
-                                        <li data-step='P' ${
-                                            item.statusTarefa === 'P'
-                                                ? ' class="atual"'
-                                                : ''
-                                        }></li>
-                                    </ul>
-                                </div>
-                                <p>  
-                                <span id='mensagem-task'>${item.mensagem}</span>
-                                </p>
-                                <p>${
-                                    item.update !== undefined
-                                        ? `<p>
-                                        Data Atualização:
-                                        <span id='atualizacao-data' class='data_span'>${item.update}</span>
-                                    </p>`
-                                        : ''
-                                }
-                                </p>
-                            </div>
                         </div>
-                        </div>
-                        `;
-                    // Adicione o código JavaScript aqui, após a criação dos elementos do cartão
-                    const progressSteps =
-                        divCard.querySelectorAll('.progressbar li');
-                    const selectedStep = item.statusTarefa;
-
-                    let activate = false;
-
-                    progressSteps.forEach((step) => {
-                        const stepValue = step.getAttribute('data-step');
-
-                        if (stepValue === selectedStep) {
-                            activate = true;
-                        }
-
-                        if (activate) {
-                            step.classList.add('active');
-                        } else {
-                            step.classList.remove('active');
-                        }
-                    });
+                    `;
 
                     // click no botão de editar
-                    const meuBotao = divCard.querySelector('#meuBotao');
+                    const meuBotao = divCard.querySelector('.fa-regular');
                     meuBotao.addEventListener('click', function () {
-                        console.log(
-                            'Clicou no botão do card com índice:',
-                            index
-                        );
-                        console.log('Dados do card:', item);
+                        const cardIndex = this.getAttribute('data-card-index');
+                        const itemClicado = tarefasDoFirebase[cardIndex];
+
+                        const modal = document.getElementById('modalCard');
+                        const modalNomeTarefa =
+                            modal.querySelector('#nomeTarefa');
+                        const modalOrigemProjeto =
+                            modal.querySelector('#origemProjeto');
+                        const modalStatusTarefa =
+                            modal.querySelector('#statusTarefa');
+                        const modalMensagem = modal.querySelector('#mensagem');
+                        const modalAutorTarefa =
+                            modal.querySelector('#autorTarefa');
+
+                        // Preencha os campos do modal com os dados do item clicado
+                        modalNomeTarefa.value = itemClicado.nomeTarefa;
+                        modalOrigemProjeto.value = itemClicado.origemProjeto;
+                        modalStatusTarefa.value = itemClicado.statusTarefa;
+                        modalMensagem.value = itemClicado.mensagem;
+                        modalAutorTarefa.value = itemClicado.autorTarefa;
+
+                        modal.style.display = 'block';
                     });
 
                     divTarefas.append(divCard);
