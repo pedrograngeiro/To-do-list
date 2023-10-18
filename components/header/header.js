6c5ec1c078c03ae584f5c7ba549edd419450d307
Pedro Grangeiro <pedrop.grangeiro@gmail.com>
2023-10-18 17:23:41 -0300

    Criando tela de login com modal

--- components/header/header.js
+++ components/header/header.js
@@ -4,9 +4,33 @@ document.addEventListener('DOMContentLoaded', function () {
         .then((header) => {
             const headerElement = document.getElementById('header');
             headerElement.innerHTML = header;
-            const body = document.querySelector('body')
+            const body = document.querySelector('body');
             const darkModeToggle = document.getElementById('liga_desliga');
-            darkModeToggle.addEventListener('click', toggleDarkMode);
+
+            const openLoginModalButton = document.getElementById(
+                'openLoginModalButton'
+            );
+            const modal = document.getElementById('modal');
+            const modalContent = document.getElementById('modalContent');
+
+            openLoginModalButton.addEventListener('click', function () {
+                // Carregar o conteúdo do login.html usando uma requisição fetch
+                fetch('components/login/login.html')
+                    .then((response) => response.text())
+                    .then((loginContent) => {
+                        modalContent.innerHTML = loginContent;
+                        modal.style.display = 'block';
+                    })
+                    .catch((error) => {
+                        console.error('Error loading login content:', error);
+                    });
+            });
+
+            const closeModalButton =
+                document.getElementById('closeModalButton');
+            closeModalButton.addEventListener('click', function () {
+                modal.style.display = 'none';
+            });
 
             function toggleDarkMode() {
                 darkModeToggle.classList.toggle('dark-mode');
