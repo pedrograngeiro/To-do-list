eeb3b90efb969b4e24e6630e180910d611e082a9
Pedro Grangeiro <pedrop.grangeiro@gmail.com>
2024-03-06 15:19:26 -0300

    Alterando distancia a esquerda do sidebar no painel e implementando redirecionamento após login

--- assets/js/checkLogin.js
+++ assets/js/checkLogin.js
@@ -11,11 +11,13 @@ function checkLoginStatus() {
             if (user) {
                 // Usuário está logado, adicione a classe 'active' ao elemento
                 statusLogin.classList.add('active');
-                console.log('Usuário está logado', user);
+                // console.log('Usuário está logado', user);
+                isUserLoggedIn();
             } else {
                 // Usuário não está logado, remova a classe 'active' do elemento
                 statusLogin.classList.remove('active');
-                console.log(user, 'Usuário não está logado');
+                // console.log(user, 'Usuário não está logado');
+                isUserLoggedIn();
             }
         },
         function (error) {
@@ -25,3 +27,15 @@ function checkLoginStatus() {
 }
 
 export { checkLoginStatus };
+
+function isUserLoggedIn() {
+    const user = firebase.auth().currentUser;
+    console.log('passou por antes do if');
+    if (window.location.href.includes('login.html')) {
+        console.log('Você está na página de login');
+        if (user) {
+            // O usuário está logado
+            window.location.href = '/index.html';
+        }
+    }
+}
